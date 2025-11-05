d72427b5788249af9bace4830db22a530f1300f8
RaccoonLasagna <102906286+RaccoonLasagna@users.noreply.github.com>
2025-11-05 06:24:12 +0000

    Increased Player Friendlness
    - changed some texture to more closely resemble the actual item
    - enlarged action prompt on the bottom right of the screen
    - recipe shows item names

--- scenes/game_objects/stations/fridge.gd
+++ scenes/game_objects/stations/fridge.gd
@@ -1,24 +1,29 @@
 extends Station
 
 @export var selector: Sprite2D
+@onready var tilemap: TileMap = self.get_parent().get_parent().get_child(0)
+
 var index := 0
 var ingredients := ["potato", "haddock", "turnip", "pluck", "ox_bung", "dough", "mars"]
 
 func toggle():
-	if selector.visible:
+	if selector.visible: #close fridge
 		selector.hide()
 		index = 0
 		selector.region_rect.position.y = 0
-	else:
+		#tilemap.set_cell()
+	else: # open fridge
 		selector.show()
 
 func next():
+	print("tile: ", tilemap.local_to_map(position))
 	if index < 6:
 		index += 1
 		selector.region_rect.position.y += 32
 	else:
 		index = 0
 		selector.region_rect.position.y = 0
+	
 func previous():
 	if index > 0:
 		index -= 1
